8a3971fce2479f6822fb3fd71b9666521458916a
zhengpenghui <zhengpenghui@qingting.fm>
2019-01-31 17:14:27 +0800

    feat(replace): Update `search` input of repalce function.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function replaceChunk (source) {
 
   let newSource = source
 
-  newSource = newSource.replace(/require[(]([^'"])/g, '__non_webpack_require__($1')
+  newSource = newSource.replace(/require[(]([\`"])/g, '__non_webpack_require__($1')
 
   this.callback(null, newSource)
 }
