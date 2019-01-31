0856ed66816e3d97136f0aceeb69c661519c3494
zhengpenghui <zhengpenghui@qingting.fm>
2019-01-31 17:01:55 +0800

    Init(project): Init settings.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webpack Dynamic Require Loader
+
+> Why we need it?
+
+> Webpack cannot dynamic require a request with an expression in it. That's why we make this loader.
---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function replaceChunk (source) {
+  this.cacheable()
+
+  let newSource = source
+
+  newSource = newSource.replace(/require[(]([^'"])/g, '__non_webpack_require__($1')
+
+  this.callback(null, newSource)
+}
+
+module.exports = replaceChunk
